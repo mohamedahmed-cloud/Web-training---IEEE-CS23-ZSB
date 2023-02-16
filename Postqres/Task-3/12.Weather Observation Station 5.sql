72d2da4cada39a602bccd37c8f32b46b76e3135c
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2023-02-16 12:49:08 +0200

    Done

--- Postqres/Task-3/12.Weather Observation Station 5.sql
+++ Postqres/Task-3/12.Weather Observation Station 5.sql
@@ -5,5 +5,6 @@
 
 -- This problem I solved from 6 month give me correct answer know give me a wrong answer 
 -- I try most of solution by no way 😅
+
 select city ,length(city) from station group by city order by length(city),city limit 1;
 select city, length(city) from station group by city order by length(city),city desc limit 1;
