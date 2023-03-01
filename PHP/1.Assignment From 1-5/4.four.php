e99d80930e543805efff6431b058819cf888e730
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2023-03-01 18:58:14 +0200

    Add Assignment From 1 -5

---
+++ PHP/1.Assignment From 1-5/4.four.php
@@ -0,0 +1,9 @@
+<?php
+
+// I don't understand what mean by true or false but the right thing to make a comment is /* */ or # or //
+
+// ## First Comment             Flase
+# // # Second Comment           False
+/* /* /* Third Comment */ //    False
+////// Fourth Comment           False
+?>
